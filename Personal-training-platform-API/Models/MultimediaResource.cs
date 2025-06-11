eb883bdca479823ee4f746c0713de1580fe26fde
57R0LL3R <drcecontre@gmail.com>
2025-06-11 16:59:30 -0500

    Add models for code-first mapping

---
+++ Personal-training-platform-API/Models/MultimediaResource.cs
@@ -0,0 +1,15 @@
+﻿namespace Personal_training_platform_API.Models
+{
+    public class MultimediaResource
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Type { get; set; } // "Video", "Image", "Text"
+        public string Url { get; set; }
+        public string Description { get; set; }
+
+        public Guid ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+    }
+
+}
