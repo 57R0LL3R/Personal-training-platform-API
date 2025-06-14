e23990b033aa4741608296994708456cdccb6cdd
57R0LL3R <drcecontre@gmail.com>
2025-06-13 19:33:17 -0500

    First methods in interface service

--- Personal-training-platform-API/Services/Interfaces/IProfileService.cs
+++ Personal-training-platform-API/Services/Interfaces/IProfileService.cs
@@ -1,6 +1,17 @@
-﻿namespace Personal_training_platform_API.Services.Interfaces
+﻿using Personal_training_platform_API.Models;
+
+namespace Personal_training_platform_API.Services.Interfaces
 {
     public interface IProfileService
     {
+        public List<Profile> GetProfiles();
+        public Profile GetProfileById(int id);
+        public Profile UpdateProfile(Profile profile, Guid id);
+        public string DeleteProfile(Profile profile);
+
+        public Profile PostProfile(Profile profile);
+
+
+
     }
 }
