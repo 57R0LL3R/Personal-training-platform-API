7d0a6472ad095acd52a44424332de76b584feff9
57R0LL3R <drcecontre@gmail.com>
2025-06-17 19:06:30 -0500

    Change names in services and fix bugs

---
+++ Personal-training-platform-API/Services/Implement/UserService.cs
@@ -0,0 +1,8 @@
+﻿using Personal_training_platform_API.Services.Interfaces;
+
+namespace Personal_training_platform_API.Services.Implement
+{
+    public class UserService:IUserService
+    {
+    }
+}
--- Personal-training-platform-API/Services/Implement/UserServices.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Personal_training_platform_API.Services.Implement
-{
-    public class UserServices
-    {
-    }
-}
