0418d825736dca7f84e418af4db3d9ea7afd19b2
57R0LL3R <drcecontre@gmail.com>
2025-06-26 18:34:33 -0500

    Add new response model

---
+++ Personal-training-platform-API/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Personal_training_platform_API.Models
+{
+    public class Response
+    {
+        public int CodeReponse { get; set; }
+        public string Message { get; set; }
+        public dynamic Data{get;set;}
+    }
+}
